2ffa4e2281c14fb46c9257ed18687111a31c0b64
Yoonyoung Heo <gjdbsdud1013@hanmail.net>
2019-07-15 17:55:54 +0900

    Solve SWEA 20190715

---
+++ SWEA_java/1204_최빈수_구하기.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		int[] arr = new int[N];
+		for (int i = 0; i < N; ++i) {
+			arr[i] = scan.nextInt();
+		}
+		Arrays.sort(arr);
+		System.out.println(arr[N/2]);
+	}
+}
